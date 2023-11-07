8aea8f50f2e7c3e502328dea0f97e0c9a92ef7a9
Tianming Liu <ucfntl0@ucl.ac.uk>
2023-11-07 18:28:58 +0000

    LSOA Searcn Query Test Complete

--- data/london lsoa csv/LSOA Spatial Boundary.sql
+++ data/london lsoa csv/LSOA Spatial Boundary.sql
@@ -1,22 +1,12 @@
--- LOAD DATA LOCAL INFILE '/Users/tianming_liu/00Self/CE/0017 Web Architecture/Final/CE-Web-Architecture-Tech-Titans/data/london lsoa csv/LSOA London.csv' 
--- INTO TABLE LSOA_GEO 
--- FIELDS TERMINATED BY ',' 
--- ENCLOSED BY '"' 
--- LINES TERMINATED BY '\n' IGNORE 1 LINES (LSOA_Code, LSOA_Name, POPDEN, @var4) SET polygon_data = ST_GeomFromText(@var4);
+-- CREATE TABLE `ucfntl0`.`lsoa` (
+--   `LSOA_Code` VARCHAR(255) NOT NULL,
+--   `LSOA_Name` VARCHAR(255) NULL,
+--   `POPDEN` FLOAT NULL,
+--   `geometry` GEOMETRY NULL,
+--   PRIMARY KEY (`LSOA_Code`));
 
-
--- LOAD DATA LOCAL INFILE 'path/to/folder/FILE' 
--- INTO TABLE photo_locations 
--- FIELDS TERMINATED BY ',' 
--- ENCLOSED BY '"' 
--- LINES TERMINATED BY '\n' IGNORE 1 LINES (pid, lat, lon, @var4) SET coords = ST_GeomFromText(@var4);
-
--- SELECT * FROM LSOA_GEO
-
--- Create a new column for geometry in WGS84 Coodinate System
--- ALTER TABLE LSOA_GEO ADD COLUMN wgs84_geom GEOMETRY;
-
--- Transfrom OSGB36 coords to WGS84
-UPDATE LSOA_GEO
-SET wgs84_geom = ST_Transform(polygon_data, 4326)
-WHERE ST_SRID(polygon_data) = 27700;
+LOAD DATA LOCAL INFILE '/Users/tianming_liu/00Self/CE/0017 Web Architecture/Final/CE-Web-Architecture-Tech-Titans/data/london lsoa csv/LSOA_London_4326.csv' 
+INTO TABLE lsoa 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES (LSOA_Code, LSOA_Name, POPDEN, @var4) SET geo = ST_GeomFromText(@var4);
---
+++ tests/backend/crime api/lsoa search.sql
@@ -0,0 +1,8 @@
+-- --Create a sql query for lsoa searcn in certain lat and lon
+-- SELECT LSOA_Code
+-- FROM lsoa
+-- WHERE ST_Contains(geo, POINT(-0.12687241011485248, 51.51919313380855));
+
+SELECT LSOA_Code
+FROM lsoa
+WHERE ST_Distance_Sphere(ST_Centroid(geo), POINT(-0.12687241011485248, 51.51919313380855)) <= 500;
