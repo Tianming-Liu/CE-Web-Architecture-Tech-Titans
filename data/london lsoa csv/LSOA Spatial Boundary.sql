53b51f416e71f5061827a185564e2bcb196ed62d
Tianming Liu <ucfntl0@ucl.ac.uk>
2023-11-08 00:46:02 +0000

    Crime Data API Complete

--- data/london lsoa csv/LSOA Spatial Boundary.sql
+++ data/london lsoa csv/LSOA Spatial Boundary.sql
@@ -5,8 +5,9 @@
 --   `geometry` GEOMETRY NULL,
 --   PRIMARY KEY (`LSOA_Code`));
 
-LOAD DATA LOCAL INFILE '/Users/tianming_liu/00Self/CE/0017 Web Architecture/Final/CE-Web-Architecture-Tech-Titans/data/london lsoa csv/LSOA_London_4326.csv' 
-INTO TABLE lsoa 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n' IGNORE 1 LINES (LSOA_Code, LSOA_Name, POPDEN, @var4) SET geo = ST_GeomFromText(@var4);
+-- LOAD DATA LOCAL INFILE '/Users/tianming_liu/00Self/CE/0017 Web Architecture/Final/CE-Web-Architecture-Tech-Titans/data/london lsoa csv/LSOA_London_4326.csv' 
+-- INTO TABLE lsoa 
+-- FIELDS TERMINATED BY ',' 
+-- ENCLOSED BY '"' 
+-- LINES TERMINATED BY '\n' IGNORE 1 LINES (LSOA_Code, LSOA_Name, POPDEN, @var4) SET geo = ST_GeomFromText(@var4);
+
--- tests/backend/crime api/crime api test.js
+++ tests/backend/crime api/crime api test.js
@@ -29,8 +29,8 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-//  API EndPoint to get crime data around specific location - /data/51.1/0.0/30 
-app.get('/data/:lat/:lon/:radius', function (req, res) {
+//  API EndPoint to get lsoa block around specific location - /data/51.1/0.0/30 
+app.get('/lsoa/:lat/:lon/:radius', function (req, res) {
 
       // Alows data to be downloaded from the server with security concerns
       res.header("Access-Control-Allow-Origin", "*");
@@ -65,6 +65,41 @@ app.get('/data/:lat/:lon/:radius', function (req, res) {
                 res.send("");
         }
 });
+
+//  API EndPoint to get crime data within certain lsoa geometry - /crime/lsoaCode 
+app.get('/crime/:lsoaCode', function (req, res) {
+
+        // Alows data to be downloaded from the server with security concerns
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-WithD");
+        // If all the variables are provided connect to the database
+        if(req.params.lsoaCode != ""){
+                 
+                  // Parse the values from the URL into numbers for the query
+                  var lsoaCode = parseFloat(req.params.lsoaCode);  
+  
+                  // SQL Statement to run
+                  var sql = "SELECT Crime_Category, SUM(\`2021_10\`+\`2021_11\`+\`2021_12\`+\`2022_01\`+\`2022_02\`+\`2022_03\`+\`2022_04\`+\`2022_05\`+\`2022_06\`+\`2022_07\`+\`2022_08\`+\`2022_09\`+\`2022_10\`+\`2022_11\`+\`2022_12\`+\`2023_01\`+\`2023_02\`+\`2023_03\`+\`2023_04\`+\`2023_05\`+\`2023_06\`+\`2023_07\`+\`2023_08\`+\`2023_09\`) AS TotalCrimeCount FROM crime_data WHERE LSOA_Code = 'E0"+lsoaCode+"' GROUP BY Crime_Category;";
+
+
+                  // Log it on the screen for debugging
+                  console.log(sql);
+  
+                  // Run the SQL Query
+                  connection.query(sql, function(err, rows, fields) {
+                          if (err) console.log("Err:" + err);
+                          if(rows != undefined){
+                                  // If we have data that comes bag send it to the user.
+                                  res.send(rows);
+                          }else{
+                                  res.send("");
+                          }
+                  });
+          }else{
+                  // If all the URL variables are not passed send an empty string to the user
+                  res.send("");
+          }
+  });
 
 
 // Setup the server and print a string to the screen when server is ready
