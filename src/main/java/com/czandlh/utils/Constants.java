d887822d3079ded45372fb952a216ccb8542e6dc
DoubleU-ANG <100694831+DoubleU-ANG@users.noreply.github.com>
2023-11-15 14:04:23 +0000

    Delete src/main directory

--- src/main/java/com/czandlh/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.czandlh.utils;
-
-public class Constants {
-
-    public static final String AUTHORIZATION = "Authorization";
-
-    public static final String CURRENT_USER = "current_user";
-
-}
